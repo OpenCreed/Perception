544e6df3aa6a660239ef6fee40bc91203a7fe035
Balaji Jayaraman <balaji.jayaraman@airbus.com>
2020-09-13 19:51:43 +0530

    default arguments instead of overloading

--- JT_File.h
+++ JT_File.h
@@ -7,26 +7,19 @@ class JT_File
 	std::ifstream file;
 
 public:
-	inline void open(std::string filepath)
+	void open(std::string filepath)
 	{
 		file.open(filepath, std::ios::binary);
 	}
 
 	template <typename T>
-	inline void read(T& object, int offset = NULL)
+	void read(T& object, int offset = NULL)
 	{
-		if (offset == NULL)
-		{
-			file.read((char*)&object, sizeof(object));
-		}
-		else
-		{
-			file.seekg(offset);
-			file.read((char*)&object, sizeof(object));
-		}
+		if (offset) file.seekg(offset);
+		file.read((char*)&object, sizeof(object));
 	}
 
-	inline ~JT_File()
+	~JT_File()
 	{
 		file.close();
 	}
